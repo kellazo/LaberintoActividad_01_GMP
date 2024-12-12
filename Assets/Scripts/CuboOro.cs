c80ee7699a4ed8140ca04f7f795c4dfe2efe47d8
Guillem Muñoz <gilli11519@gmail.com>
2024-12-12 23:26:38 +0100

    Update CuboOro.cs

--- Assets/Scripts/CuboOro.cs
+++ Assets/Scripts/CuboOro.cs
@@ -17,4 +17,17 @@ public class CuboOro : MonoBehaviour
        // Vector3 direccionRotacion = Vector3.up; //  es lo mismo que Vector3 direccionRotacion = new Vector3(0, 1, 0);
         transform.Rotate(Vector3.up * velocidadRotacion * Time.deltaTime, Space.World); // rotacion en la cordenada Y positiva global (self por defecto es local)
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+       // He tocado algo (solo pasa 1 vez en el primer ciclo de fisicas) no en el primer frime (diferenciar entre el update y el ciclo de fisicas )
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        // se activa mientras estas dentro de la entidad que estas en contacto
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        // Dejo de tocar algo   (es el ultimo ciclo de fisicas en la que etuvistes en contacto se va a ejecutar
+    }
 }
