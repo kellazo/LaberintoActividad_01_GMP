126f4f96c1d27e175cf3c019d84dfcc5bb7bb58c
Guillem Muñoz <gilli11519@gmail.com>
2024-12-14 19:43:11 +0100

    last class ending CharacterController
    I have recently watched the last class session. Ends with the CharacterController component adding the manual gravity because of the  Move() it's manual. Reminder the movement is kinematic.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,10 +8,12 @@ public class PlayerController : MonoBehaviour
 {
     // Velocidad de avance y giro. Ajustables desde el Inspector.
     [Tooltip("Factor multiplicador para la velocidad de movimiento hacia adelante/atrás.")]
-    public float moveSpeed = 3.0f;
-
-    [Tooltip("Factor multiplicador para la velocidad de rotación (giro).")]
-    public float rotateSpeed = 120.0f; // grados por segundo
+    [SerializeField] private float moveSpeed; //= 3.0f;
+    [Tooltip("Factor multiplicador para la gravedad de movimiento hacia abajo (y)")]
+    [SerializeField] private float gravitySpeed; //= 3.0f;
+    [Tooltip("Factor multiplicador para la velocidad de rotación (giro) Grados por segundo.")]
+    [SerializeField] private float rotateSpeed; //= 120.0f; // grados por segundo
+    private Vector3 movimientoVertical;
 
     // Referencia al CharacterController
     private CharacterController controller;
@@ -30,28 +32,33 @@ public class PlayerController : MonoBehaviour
         // movimiento de un cuerpo de forma cinemática (sin fisicas)
 
         // getAxis tiene un impulso hasta llegar a 1 o -1 y GetAxisRaw es el absoluto.
-        float hInput = Input.GetAxis("Horizontal"); // A/D o Flechas Izq/Der  -1, 0, 1
-        float vInput = Input.GetAxis("Vertical");     // W/S o Flechas Arriba/Abajo -1, 0 , 1
+        float hInput = Input.GetAxisRaw("Horizontal"); // A/D o Flechas Izq/Der  -1, 0, 1
+        float vInput = Input.GetAxisRaw("Vertical");     // W/S o Flechas Arriba/Abajo -1, 0 , 1
 
         // Usamos Time.deltaTime para que la rotación no dependa del framerate.
-        transform.Rotate(0, hInput * rotateSpeed * Time.deltaTime, 0);
+        //transform.Rotate(0, hInput * rotateSpeed * Time.deltaTime, 0);
 
         // Creamos un vector de movimiento local: hacia adelante lo que indica "vertical" y normalizamos (vector unitario) para que la diagonal entre "dentro del círculo"
         Vector3 moveDirection = new Vector3(hInput, 0, vInput).normalized;
         // transform.Translate(moveDirection * moveSpeed * Time.deltaTime); //Manera en que lo ha dado en clase Fernando, es lo mismo que transform.position += moveDirection * moveSpeed * Time.deltaTime;
         // Transformar este vector local al espacio global teniendo en cuenta la rotación actual del personaje.
-        moveDirection = transform.TransformDirection(moveDirection);
+       // moveDirection = transform.TransformDirection(moveDirection);
 
         // Multiplicamos por la velocidad de movimiento y por deltaTime para que el desplazamiento sea consistente.
         moveDirection = moveDirection * moveSpeed * Time.deltaTime;
 
         // Aplicar el movimiento al CharacterController
         controller.Move(moveDirection);
+
+        AplicarGravedad();
     }
     private void OnTriggerEnter(Collider other)
     {
-        // He tocado algo (solo pasa 1 vez en el primer ciclo de fisicas) no en el primer frime (diferenciar entre el update y el ciclo de fisicas )
-        Destroy(other.gameObject);
+        if (other.gameObject.CompareTag("CuboOro"))
+        {
+            // He tocado algo (solo pasa 1 vez en el primer ciclo de fisicas) no en el primer frime (diferenciar entre el update y el ciclo de fisicas )
+            Destroy(other.gameObject);
+        }
     }
     private void OnTriggerStay(Collider other)
     {
@@ -60,5 +67,10 @@ public class PlayerController : MonoBehaviour
     private void OnTriggerExit(Collider other)
     {
         // Dejo de tocar algo   (es el ultimo ciclo de fisicas en la que etuvistes en contacto se va a ejecutar
+    }
+    private void AplicarGravedad()
+    {
+        movimientoVertical.y += gravitySpeed * Time.deltaTime; // es como el impulso del salto.
+        controller.Move(movimientoVertical *  Time.deltaTime); // hay dos time delta time porque la aceleracion es metros / por segundos al cuadrado
     }
 }
--- Assets/Scripts/PlayerDynamic.cs
+++ Assets/Scripts/PlayerDynamic.cs
@@ -6,8 +6,12 @@ using UnityEngine;
 public class PlayerDynamic : MonoBehaviour
 {
     [SerializeField] private float fuerza;
+    //[SerializeField] private float fuerzaSalto;
+    [SerializeField] private float ratioCargaSalto;
+    private float fuerzaSalto;
     private Rigidbody rb;
     private float hInput, vInput;
+    private float fuerzaSaltoMaxima;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,14 +24,44 @@ public class PlayerDynamic : MonoBehaviour
         
         hInput = Input.GetAxis("Horizontal");
         vInput = Input.GetAxis("Vertical");
-        
+
+        // salto simple
+        /* if (Input.GetKeyDown(KeyCode.Space)) // En el primer frame de la tecla espacio saltar con impulse en la Y
+         {
+             rb.AddForce(Vector3.up * fuerzaSalto, ForceMode.Impulse);
+         }*/
+        //salto acumulado
+        if (Input.GetKey(KeyCode.Space)) // mientras mantengo el espacio
+        {
+            fuerzaSalto += ratioCargaSalto * Time.deltaTime; // por segundo cuando tu sumas algo, la fuerza de salto se va cargando a 3 por segundo
+            fuerzaSalto = Mathf.Min(fuerzaSalto, fuerzaSaltoMaxima);
+            /*if(fuerzaSalto > fuerzaSaltoMaxima)
+            {
+                fuerzaSalto = fuerzaSaltoMaxima;
+            }*/
+        }
+        if (Input.GetKeyUp(KeyCode.Space)) // Cuando sueltas el espacio
+        {
+            rb.AddForce(Vector3.up * fuerzaSalto, ForceMode.Impulse); // haces el salto de impulso y dejas la fuerza a 0 importante
+            fuerzaSalto = 0f;
+        }
+
 
     }
-    // Ciclo de fisicas: Es donde se gestinonan los calculos físicos.
-    // Update vs Fixed: SON INDEPENDIENTES. Es fijo, pk se ejecuta en un ratio constante. Por tanto no afecta los frames(delta time). A que ratio va?
-    private void FixedUpdate()
+    // Ciclo de fisicas: Es donde se gestinonan los calculos físicos. // SOLO fisicas continuas, que necesitan cálculos a lo largo de un tiempo.
+    // Update vs Fixed: SON INDEPENDIENTES. Es fijo, pk se ejecuta en un ratio constante. Por tanto no afecta los frames(delta time). A que ratio va? (cada cuanto se ejecuta?) En Project Settings Time Fixed Timestep
+
+    private void FixedUpdate() // solo meter físicas continuas (todo lo que se necesita recalcular en el tiempo) El force si, el impulse no. 
     {
         // movimiento de un cuerpo de forma dinamica (fisicas)
         rb.AddForce(new Vector3(hInput, 0, vInput).normalized * fuerza, ForceMode.Force); // aqui no podemos meter el delta time que es por segundo pero la fuerza es en Newtons.
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("CuboOro"))
+        {
+            Destroy(other.gameObject);
+        }
     }
 }
