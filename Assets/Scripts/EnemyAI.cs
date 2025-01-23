5e6c4a80e508b71e87d9e9af5282f7c44751580c
Guillem Muñoz <gilli11519@gmail.com>
2025-01-23 20:08:29 +0100

    spawner

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -10,14 +10,21 @@ public class EnemyAI : MonoBehaviour
 
     private NavMeshAgent agent;
 
+    // preparando mis cosas pero no depende de otros, se ejecuta una sola vez. Nada mas comenzar el juego estes o no estes activo
     private void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
     }
-    // Start is called before the first frame update
-    void Start()
+
+    // cuando nos hemos activado (se puede repetir)
+    private void OnEnable()
     {
         
+    }
+    // 1 sola vez, util para preparar otras entidades dependientes. Se ejecuta despues del awake y onenable
+    void Start()
+    {
+        player = GameObject.FindObjectOfType<PlayerController>().gameObject;
     }
 
     // Update is called once per frame
