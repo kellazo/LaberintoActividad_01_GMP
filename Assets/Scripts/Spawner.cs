cad1015b0c7f85c617ae40a6b345180e5f8de08c
Guillem Muñoz <gilli11519@gmail.com>
2025-01-23 20:28:59 +0100

    spawnear corutina

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
+
 
 public class Spawner : MonoBehaviour
 {
@@ -10,12 +12,31 @@ public class Spawner : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(enemyPrefab, transform.position, Quaternion.identity); //Quaternion.Euler(0,0,0) = Quaternion.identity, matriz identidad (diagonal de unos), nulo
+        
+
+        StartCoroutine(Spawnear());
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    //Metodo ejecutado por intervalos. (Rutina)
+    private IEnumerator Spawnear()
+    {
+        while (true)
+        {
+
+            Instantiate(enemyPrefab, transform.position, Quaternion.identity); //Quaternion.Euler(0,0,0) = Quaternion.identity, matriz identidad (diagonal de unos), nulo
+           // UnityEngine.Debug.Log("Verde");
+            yield return new WaitForSeconds(2f);
+            //Espera 2 segundos
+            //UnityEngine.Debug.Log("Amarillo");
+            //Espera 2 segundos
+            //yield return new WaitForSeconds(2f);
+            //UnityEngine.Debug.Log("Rojo");
+        }
     }
 }
